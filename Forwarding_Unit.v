2ca4bd3d0479f37c07e4ca6ea962b74fe0504cf6
ananyaPal2001 <ananyapal@iisc.ac.in>
2024-03-24 16:02:50 +0530

    Data Forwarding - 3
    Adding separate MEM hazard and EX Hazard, Correcting the output type from wire to reg.

--- Forwarding_Unit.v
+++ Forwarding_Unit.v
@@ -5,12 +5,24 @@ module Forwarding_Unit (
     input wire [4:0] mem_wb_reg_RD,
     input wire ex_mem_regwrite,
     input wire mem_wb_regwrite,
-    output wire [1:0] fwd_A,
-    output wire [1:0] fwd_B
+    output reg [1:0] fwd_A,
+    output reg [1:0] fwd_B
 );
 
+    fwd_A = 00;
+    fwd_B = 00;
+
     always@(*) begin
-        // forwarding condition
+        // forwarding condition for EX hazard
+        if(ex_mem_regwrite && (ex_mem_reg_RD != 0)) begin
+            if((ex_mem_reg_RD == reg_RS1)) begin
+                fwd_A = 10;
+            end
+            if((ex_mem_reg_RD == reg_RS2)) begin
+                fwd_B = 10;
+            end
+        end
+        // forwarding condition for MEM hazard
         if(mem_wb_regwrite && (mem_wb_reg_RD != 0)) begin
             if (!(ex_mem_regwrite && (ex_mem_reg_RD != 0))) begin
                 if((ex_mem_reg_RD == reg_RS1) && (mem_wb_reg_RD == reg_RS1)) begin
